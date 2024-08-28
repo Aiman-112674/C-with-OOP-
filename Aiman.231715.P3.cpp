946619614c2bd4294ce806ce8dc6bd4c5db2ffa6
Aiman-112674 <aimanzafar348@gmail.com>
2024-08-28 01:54:39 -0700

    Add files via upload

---
+++ Aiman.231715.P3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 10;
+	int b = 30;
+	cout<<"orignal value of a ="<<a <<endl;
+	a=a+b;
+	cout<<"after sum value of a ="<<a;
+	"returns 0";
+}
